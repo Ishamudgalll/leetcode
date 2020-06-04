885f231b99fb1eefd38bfa939f81d50db6d2abc1
Ishamudgalll <47916019+Ishamudgalll@users.noreply.github.com>
2020-06-04 10:14:32 +0530

    Create set_vs_map1.cpp

---
+++ set_vs_map1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+    set<int> s1; 
+    s1.insert(2); 
+    s1.insert(5); 
+    s1.insert(3); 
+    s1.insert(6); 
+  
+    cout << "Elements in set:\n"; 
+    for (auto it : s1) 
+        cout << it << " "; // Sorted 
+  
+    return 0; 
+} 
